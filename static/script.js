1d6463506306dff6687ff93af3acf374a3d69582
JohnyLilMoney <dinnerbone566@gmail.com>
2026-08-26 09:44:57 +0300

    Add maxSize parameter to iframe source URL

--- static/script.js
+++ static/script.js
@@ -249,9 +249,9 @@ async function loadPhoneStream() {
     const host = window.location.host; // ts.johnylilmoney.nl
     const wsUrl = `wss://${host}/phone/?action=proxy-adb&remote=tcp:8886&udid=${encodeURIComponent(udid)}`;
 
-    iframe.src = `/phone/#!action=stream&udid=${encodeURIComponent(udid)}&player=webcodecs&ws=${encodeURIComponent(wsUrl)}&fitToScreen=true`;
+    iframe.src = `/phone/#!action=stream&udid=${encodeURIComponent(udid)}&player=webcodecs&ws=${encodeURIComponent(wsUrl)}&fitToScreen=true&maxSize=720`;
 
-    requestAnimationFrame(() => {
+	requestAnimationFrame(() => {
         streamSection.classList.add('stream-active');
     });
 }
